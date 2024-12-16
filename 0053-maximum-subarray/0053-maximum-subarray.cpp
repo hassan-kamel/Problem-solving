5f68e7c9c72fb5c3f573d23464e48842aa13eb59
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-12-16 05:53:48 +0200

    Time: 88 ms (5.56%), Space: 67.7 MB (99.96%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& A) {
+        int sum = 0,best=A[0];
+        for(int i=0;i<A.size();i++){
+            sum=max(A[i],A[i]+sum);
+            best=max(best,sum);
+        }
+        return best;
+    }
+};
