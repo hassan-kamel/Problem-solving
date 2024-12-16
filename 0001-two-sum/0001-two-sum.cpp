c77f688170dd78dc3133d62f707936014b31a018
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-12-16 07:24:19 +0200

    Time: 4 ms (64.47%), Space: 14.8 MB (21.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        unordered_map<int, pair<int, int>> nums_hash;
-        int index = 0;
-        for (int num : nums) {
-            auto it = nums_hash.find(num);
-            if (it != nums_hash.end() && nums_hash[num].first + num == target &&
-                nums_hash[num].second != index) {
-                result = {nums_hash[num].second, index};
-                break;
+        unordered_map<int, int> num_index;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (num_index.count(complement)) {
+                return {num_index[complement], i};
             }
-
-            int complete = target - num;
-            cout << complete << endl;
-            nums_hash[complete] = {num, index};
-
-            index++;
+            num_index[nums[i]] = i;
         }
-
-        return result;
+        return {};
     }
 };
