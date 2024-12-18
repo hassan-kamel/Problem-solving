af381706feaebc125e5967a36f437142aca5f10f
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-12-18 22:51:57 +0200

    Time: 47 ms (96.32%), Space: 81.1 MB (68.49%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int len = nums.size();
         unordered_map<int, int> map;
-        for (int i = 0; i < len; i++) {
-            if (map[nums[i]] > 0 && (((i + 1) - map[nums[i]]) <= k)) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.count(nums[i]) && i - map[nums[i]] <= k) {
                 return true;
             }
-            map[nums[i]] = i + 1;
+            map[nums[i]] = i; 
         }
-
         return false;
     }
 };
