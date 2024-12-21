b1ec9b333c3652dc90ecde9be00fe3f8d917ac9b
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-12-21 17:58:06 +0200

    Time: 2 ms (27.96%), Space: 9.8 MB (32.83%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        int len1 = str1.size(), len2 = str2.size();
-        string base = len1 > len2 ? str2 : str1;
-
-        while(true){
-            if(isGCD(base,str1) && isGCD(base,str2)) return base;
-            else if(base.size()== 1) return "";
-            else base = base.substr(0,base.size()-1);
+        // Check if they have non-zero GCD string.
+        if (str1 + str2 != str2 + str1) {
+            return "";
         }
 
-        return "";
-    }
-
-    bool isGCD(string base, string str) {
-        if (str.size() % base.size() == 0) {
-            int k = str.size() / base.size();
-            string result = "";
-            while (k--)
-                result += base;
-            if (str == result)
-                return true;
-        }
-
-        return false;
+        // Get the GCD of the two lengths.
+        int gcdLength = gcd(str1.size(), str2.size());
+        return str1.substr(0, gcdLength);
     }
 };
