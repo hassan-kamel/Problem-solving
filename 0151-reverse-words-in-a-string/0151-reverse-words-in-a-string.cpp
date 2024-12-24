e42c0ee2ea8b6c124f49970b8857c04cab894a0a
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-12-24 08:04:49 +0200

    Time: 7 ms (21.69%), Space: 23.8 MB (9.64%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string result = "";
+        string current_word = "";
+        cout << "siz: " << current_word.size() << endl;
+        for (char ch : s) {
+            if (ch == ' ') {
+                if (current_word.size() > 0) {
+                    result = result.size() ? current_word + ' ' + result
+                                           : current_word;
+                    current_word = "";
+                }
+            } else {
+                current_word += ch;
+            }
+        }
+
+        return (current_word.size() > 0
+                    ? result.size() ? current_word + ' ' + result : current_word
+                    : result);
+    }
+};
