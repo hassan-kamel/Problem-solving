aa702031948e46b1ae2fd53a2d141a0b6be7623e
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-12-26 07:35:49 +0200

    Time: 0 ms (100%), Space: 8.6 MB (18.24%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int j = 0;                           // Pointer for t
-        for (int i = 0; i < s.size(); i++) { // Pointer for s
+        int j = 0;
+        for (int i = 0; i < s.size(); i++) {
             while (j < t.size() && s[i] != t[j]) {
                 j++;
             }
             if (j == t.size())
-                return false; // Exhausted t without matching s[i]
-            j++;              // Move t pointer after matching
+                return false;
+            j++;
         }
         return true;
     }
