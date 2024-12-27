33fc93b3a4b89dac737ef79073a79ae8e2a6fe65
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-12-27 11:19:41 +0200

    Time: 14 ms (88.43%), Space: 37.5 MB (32.17%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        int len1 = nums1.size(), len2 = nums2.size();
-        unordered_map<int, int> map;
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
 
-        for (int num1 : nums1)
-            map[num1] = 1;
-        for (int i = 0; i < nums2.size(); i++) {
-            if (map[nums2[i]] > 0) {
-                map[nums2[i]] = map[nums2[i]] + 1;
-                nums2.erase(nums2.begin() + i);
-                i--;
-            }else
-                map[nums2[i]] = map[nums2[i]] + 1;
-        }
-        for (int j = 0; j < nums1.size(); j++) {
-            if (map[nums1[j]] > 1) {
-                nums1.erase(nums1.begin() + j);
-                j--;
-            } else
-                map[nums1[j]] = map[nums1[j]] + 1;
+        vector<int> diff1, diff2;
+
+        // Find elements in set1 but not in set2
+        for (int num : set1) {
+            if (set2.find(num) == set2.end()) {
+                diff1.push_back(num);
+            }
         }
 
-        return {nums1, nums2};
+        // Find elements in set2 but not in set1
+        for (int num : set2) {
+            if (set1.find(num) == set1.end()) {
+                diff2.push_back(num);
+            }
+        }
+
+        return {diff1, diff2};
     }
 };
