6d309c19de838365c99d6795a3bd43829ecc79d8
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-12-29 00:12:50 +0200

    Time: 0 ms (100%), Space: 22.3 MB (8.94%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> stk;
+        stack<int> stk; // Stack to handle asteroid collisions
         for (int aster : asteroids) {
             if (!stk.empty()) {
                 if (stk.top() > 0) {
                     if (aster > 0)
-                        stk.push(aster);
+                        stk.push(aster); // Both are moving right
                     else {
+                        // Collision: Current asteroid moving left
                         while (!stk.empty() && (aster * -1) > stk.top() &&
                                stk.top() > 0) {
-                            stk.pop();
+                            stk.pop(); // Current asteroid destroys the top
                         }
                         if (!stk.empty() && (aster * -1) == stk.top())
-                            stk.pop();
+                            stk.pop(); // Both destroy each other
                         else if (aster != 0 && (stk.empty() || stk.top() < 0))
-                            stk.push(aster);
+                            stk.push(aster); // Current asteroid survives
                     }
                 } else {
-                    stk.push(aster);
+                    stk.push(aster); // No conflict with left-moving asteroid
                 }
             } else
-                stk.push(aster);
+                stk.push(aster); // Stack is empty
         }
 
         vector<int> result;
@@ -30,7 +31,7 @@ public:
             stk.pop();
         }
 
-        reverse(result.begin(), result.end());
+        reverse(result.begin(), result.end()); // Reverse to get correct order
         return result;
     }
 };
