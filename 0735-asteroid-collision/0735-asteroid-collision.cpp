80fbbe8d77ae44abb4cc5920486e1bbaad15b220
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-12-29 00:10:40 +0200

    Time: 0 ms (100%), Space: 21.6 MB (41.2%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> stk;
-        for (int aster : asteroids) {
-            if (!stk.empty()) {
-                if (stk.top() > 0) {
-                    if (aster > 0)
-                        stk.push(aster);
-                    else {
-                        while (!stk.empty() && (aster * -1) > stk.top() &&
-                               stk.top() > 0) {
-                            stk.pop();
-                        }
-                        if (!stk.empty() && (aster * -1) == stk.top())
-                            stk.pop();
-                        else if (aster != 0 && (stk.empty() || stk.top() < 0))
-                            stk.push(aster);
-                    }
-                } else {
-                    stk.push(aster);
-                }
-            } else
-                stk.push(aster);
-        }
-
         vector<int> result;
-        while (!stk.empty()) {
-            result.push_back(stk.top());
-            stk.pop();
+        for (int aster : asteroids) {
+            while (!result.empty() && aster < 0 && result.back() > 0) {
+                if (result.back() < -aster) {
+                    result.pop_back();
+                    continue;
+                } else if (result.back() == -aster) {
+                    result.pop_back();
+                }
+                aster = 0; // Mark current asteroid as destroyed
+                break;
+            }
+            if (aster != 0) result.push_back(aster);
         }
-
-        reverse(result.begin(), result.end());
         return result;
     }
 };
