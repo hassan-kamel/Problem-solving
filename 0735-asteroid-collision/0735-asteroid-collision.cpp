7a2ddaa1bea1fafbae632686fa143c7c713093a1
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-12-29 00:15:44 +0200

    Time: 0 ms (100%), Space: 22.1 MB (20.11%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> stk; // Stack to handle asteroid collisions
+        stack<int> stk;
         for (int aster : asteroids) {
-            if (!stk.empty()) {
-                if (stk.top() > 0) {
-                    if (aster > 0)
-                        stk.push(aster); // Both are moving right
-                    else {
-                        // Collision: Current asteroid moving left
-                        while (!stk.empty() && (aster * -1) > stk.top() &&
-                               stk.top() > 0) {
-                            stk.pop(); // Current asteroid destroys the top
-                        }
-                        if (!stk.empty() && (aster * -1) == stk.top())
-                            stk.pop(); // Both destroy each other
-                        else if (aster != 0 && (stk.empty() || stk.top() < 0))
-                            stk.push(aster); // Current asteroid survives
-                    }
-                } else {
-                    stk.push(aster); // No conflict with left-moving asteroid
-                }
-            } else
-                stk.push(aster); // Stack is empty
+            while (!stk.empty() && aster < 0 && stk.top() > 0) {
+                if (abs(aster) > stk.top())
+                    stk.pop();
+                else if (abs(aster) == stk.top()) {
+                    stk.pop();
+                    aster = 0;
+                } else
+                    aster = 0;
+            }
+            if (aster != 0)
+                stk.push(aster);
         }
 
         vector<int> result;
@@ -30,8 +21,7 @@ public:
             result.push_back(stk.top());
             stk.pop();
         }
-
-        reverse(result.begin(), result.end()); // Reverse to get correct order
+        reverse(result.begin(), result.end());
         return result;
     }
 };
