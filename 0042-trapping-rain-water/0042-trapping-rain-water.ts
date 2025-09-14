50ae4c69938be37e16680c45210582e25d77893a
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2025-09-14 03:07:28 +0300

    Time: 452 ms (7.18%), Space: 56.8 MB (78.56%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.ts
@@ -0,0 +1,22 @@
+/**
+
+    eq = min(maxL,maxR) - h[i];
+  */
+
+function trap(heights: number[]): number {
+    let trappedWater = 0, maxL = 0;
+
+    heights.forEach((h, idx) => {
+        let maxR = 0;
+        let right = idx + 1;
+        while (right < heights.length) {
+            maxR = Math.max(maxR, heights[right]);
+            right++;
+        }
+
+        trappedWater += (Math.max(0, Math.min(maxR, maxL) - h));
+        maxL = Math.max(maxL, h);
+    })
+
+    return trappedWater;
+};
