2cdd6393204d3c5f094eeb5031fe785373bedd2d
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2025-09-24 21:19:42 +0300

    Time: 3 ms (50.96%), Space: 68.3 MB (97.31%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
@@ -1,13 +1,10 @@
 function findMaxAverage(nums: number[], k: number): number {
     let maxS = nums.slice(0, k).reduce((acc, curr) => acc + curr, 0), curr = maxS;
-    console.log(maxS)
     for (let i = k; i < nums.length; i++) {
-        curr += nums[i];
-        curr -= nums[i - k];
+        curr += nums[i] - nums[i - k];
 
-        maxS = curr / k > maxS / k ? curr : maxS;
+        maxS = Math.max(curr, maxS)
 
     }
-
     return maxS / k;
 };
