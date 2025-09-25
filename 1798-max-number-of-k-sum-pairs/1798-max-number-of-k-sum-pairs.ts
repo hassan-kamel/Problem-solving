09a41f1f4e3155ddd64f7e39371cae89ceb1ab6d
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2025-09-25 13:34:41 +0300

    Time: 69 ms (98.53%), Space: 73.5 MB (13.97%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.ts
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.ts
@@ -1,10 +1,11 @@
 function maxOperations(nums: number[], k: number): number {
     let comp = new Map(), maxO = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (comp.has(k - nums[i])) {
+        let curr = k - nums[i];
+        if (comp.has(curr)) {
             maxO++;
-            comp.set(k - nums[i], comp.get(k - nums[i]) - 1)
-            if (comp.get(k - nums[i]) == 0) comp.delete(k - nums[i])
+            comp.set(curr, comp.get(curr) - 1)
+            if (comp.get(curr) == 0) comp.delete(curr)
         } else {
             comp.set(nums[i], (comp.get(nums[i]) || 0) + 1)
         }
