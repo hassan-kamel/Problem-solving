31f032906a46a2a5f360ec55358d98ce1531b2f1
Hassan Mohamed Kamel <60411536+hassan-kamel@users.noreply.github.com>
2025-10-06 04:12:47 +0300

    Time: 0 ms (100%), Space: 8.1 MB (67.73%) - LeetHub

--- 0886-score-of-parentheses/0886-score-of-parentheses.cpp
+++ 0886-score-of-parentheses/0886-score-of-parentheses.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        char prev = '(';
-        int res = 0, depth = 0;
-        for (auto&& c : s) {
-            if (c == '(')
-                depth++;
-            else {
-                depth--;
-                if (prev == '(')
-                    res += pow(2, depth);
+        stack<int> st;
+        int score = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(score);
+                score = 0;
+            } else {
+                score = st.top() + max(2 * score, 1);
+                st.pop();
             }
-            prev = c;
         }
-        return res;
+        return score;
     }
 };
